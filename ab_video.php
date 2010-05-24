d2341d4152bedd02aabdd70c992c3391078e5d76
AndreasBa73 <AndreasBa73@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-24 15:56:17 +0000

    Adding first version of ab-video
    git-svn-id: https://plugins.svn.wordpress.org/ab-video/trunk@245078 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ab_video.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: AB Video
+Plugin URI: http://www.bachmaier.cc/
+Description: Allows the user to embed Youtube / Vimeo movie clips by entering a shortcode ([youtube] / [vimeo]) into the post area.
+Author: Andreas Bachmaier
+Version: 1.0
+Author URI: http://www.bachmaier.cc/
+License: GPL 2.0, @see http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+class ab_video {
+    function vimeo($atts, $content=null) {
+		extract(shortcode_atts(array(
+			'clip_id' 	=> '',
+			'width' 	=> '512',
+			'height' 	=> '288',
+		), $atts));
+
+		if (empty($clip_id) || !is_numeric($clip_id)) return '<!-- AB Video: Invalid clip_id -->';
+		if ($height && !$width) $width = intval($height * 16 / 9);
+		if (!$height && $width) $height = intval($width * 9 / 16);
+
+		return "<p><object width='$width' height='$height'><param name='allowfullscreen' value='true' />".
+    			"<param name='allowscriptaccess' value='always' />".
+    			"<param name='movie' value='http://vimeo.com/moogaloop.swf?clip_id=$clip_id&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1' />".
+    			"<embed src='http://vimeo.com/moogaloop.swf?clip_id=$clip_id&amp;server=vimeo.com&amp;show_title=1&amp;show_byline=1&amp;show_portrait=0&amp;color=&amp;fullscreen=1' type='application/x-shockwave-flash' allowfullscreen='true' allowscriptaccess='always' width='$width' height='$height'></embed></object>".
+    			"</p>";
+    }
+	
+	function youtube($atts, $content=null) {
+		extract(shortcode_atts(array(
+			'clip_id' 	=> '',
+			'width' 	=> '512',
+			'height' 	=> '288',
+		), $atts));
+		
+		if (empty($clip_id)) return '<!-- AB Video: Invalid clip_id -->';
+		if ($height && !$width) $width = intval($height * 16 / 9);
+		if (!$height && $width) $height = intval($width * 9 / 16);
+		$height += 25; // Youtube Controls
+		
+		return '<p><object width="'.$width.'" height="'.$height.'" type="application/x-shockwave-flash" data="http://www.youtube.com/v/'.$clip_id.'&fs=1&fmt=18">
+			<param name="movie" value="http://www.youtube.com/v/'.$clip_id.'&fs=1&fmt=18"></param>
+			<param name="allowFullScreen" value="true" />
+			<param name="allowscriptaccess" value="always" />
+			</object></p>';
+	}
+}
+
+add_shortcode('youtube', array('ab_video', 'youtube'));
+add_shortcode('vimeo', array('ab_video', 'vimeo'));
+
+?>
---
+++ readme.txt
@@ -0,0 +1,33 @@
+=== Youtube - Vimeo - Video ===
+Contributors: andreasb73
+Tags: video, embed, movie, shortcode, plugin, clip, vimeo, youtube
+Requires at least: 2.5
+Tested up to: 3.0
+Stable tag: 1.0
+
+Allows the user to embed Youtube or Vimeo movie clips by entering a shortcode ([youtbue] or [vimeo]) into the post area. 
+
+Video options are supported as short code attributes.
+
+== Description ==
+
+Allows the user to embed Youtube or Vimeo movie clips by entering a shortcode ([youtube] or [vimeo]) into the post area.
+
+== Installation ==
+
+1. Unzip `ab_video.zip` and upload the contained files to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Usage ==
+Vimeo:
+1. Enter the `[vimeo clip_id="XXXXXXX"]` short code into any post. `clip_id` is the number from the clip's URL (e.g. http://vimeo.com/123456)
+2. Optionally modify the clip's appearance by specifying width or height, like so: `[vimeo clip_id="XXXXXXX" width="400" height="225"]`
+3. Using empty values for either the `width` or `height`attributes will cause AB Video to calculate the proper dimension based on a 16:9 aspect ration. 
+   Example: `[vimeo clip_id="12345678" height="300" width=""]` or `[vimeo clip_id="12345678" height="" width="640"]`
+
+Youtube:
+1. Enter the `[youtube clip_id="XXXXXXX"]` short code into any post. `clip_id` is the id from the clip's URL after v= (e.g. http://www.youtube.com/watch?v=2LbpLRZwWtE)
+2. Optionally modify the clip's appearance by specifying width or height, like so: `[youtube clip_id="XXXXXXX" width="400" height="225"]`
+3. Using empty values for either the `width` or `height`attributes will cause AB Video to calculate the proper dimension based on a 16:9 aspect ration. 
+   Example: `[youtube clip_id="2LbpLRZwWtE" height="300" width=""]` or `[youtube clip_id="2LbpLRZwWtE" height="" width="640"]`
+
